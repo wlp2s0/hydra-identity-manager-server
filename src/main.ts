05944689adfde2562aa7c5dad02b899ebb772ca2
Mattia Campagna <me@mattiacampagna.com>
2021-06-25 17:03:56 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:4002',
+    origin: ['http://localhost:4002', process.env.APP_CORS_ORIGIN],
     credentials: true,
   });
   await app.listen(5002);
